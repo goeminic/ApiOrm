1dffc96109df6ebb8a0c1e40cfd9c7288c23aaaf
goeminic <gnico2127@gmail.com>
2024-09-17 08:37:27 -0300

    Instalacion de dependencias y preparacion de entorno

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+
+app.listen(6505,()=>{
+    console.log("Servidor Activo");
+});
+
